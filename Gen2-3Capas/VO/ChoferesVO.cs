f74f4b5fc8b7d245cb1aa7d8d532db5abdbd7747
Cotze <carlosrodrigoatl@gmail.com>
2022-02-08 16:30:14 -0600

    First commit

---
+++ Gen2-3Capas/VO/ChoferesVO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Gen2_3Capas.VO
+{
+    public class ChoferesVO
+    {
+        private int _IdChofer;
+        private String _Nombre;
+        private String _ApPaterno;
+        private String _ApMaterno;
+        private String _Telefono;
+        private DateTime _FechaNacimiento;
+        private String _Licencia;
+        private String _UrlFoto;
+        private bool _Disponiblidad;
+
+        public int IdChofer { get => _IdChofer; set => _IdChofer=value; }
+        public string Nombre { get => _Nombre; set => _Nombre=value; }
+        public string ApPaterno { get => _ApPaterno; set => _ApPaterno=value; }
+        public string ApMaterno { get => _ApMaterno; set => _ApMaterno=value; }
+        public string Telefono { get => _Telefono; set => _Telefono=value; }
+        public DateTime FechaNacimiento { get => _FechaNacimiento; set => _FechaNacimiento=value; }
+        public string Licencia { get => _Licencia; set => _Licencia=value; }
+        public string UrlFoto { get => _UrlFoto; set => _UrlFoto=value; }
+        public bool Disponiblidad { get => _Disponiblidad; set => _Disponiblidad=value; }
+
+        public ChoferesVO() //Constructor
+        {
+            IdChofer = 0;
+            Nombre = "";
+            ApPaterno = "";
+            ApMaterno = "";
+            Telefono = "";
+            FechaNacimiento = DateTime.Parse("1900-01-01");
+            Licencia ="";
+            UrlFoto = "";
+            Disponiblidad = false;
+        }
+
+        public ChoferesVO(DataRow dr)
+        {
+            IdChofer = int.Parse(dr["id"].ToString());
+            Nombre = dr["Nombre"].ToString();
+            ApPaterno = dr["ApPaterno"].ToString();
+            ApPaterno = dr["ApMaterno"].ToString();
+            Telefono = dr["Telefono"].ToString();
+            FechaNacimiento = DateTime.Parse(dr["FechaNacimiento"].ToString());
+            UrlFoto = dr["UrlFoto"].ToString();
+            Disponiblidad = bool.Parse(dr["Disponibilidad"].ToString());
+        }
+    }
+
+}
---
+++ Gen2-3Capas/enumeradores.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Gen2_3Capas
+{
+    public enum Marca
+    {
+        Volvo,
+        Alliance,
+        Ford,
+        [Description("Mercedes Benz")]
+        Mercedes,
+        Dina
+    }
+
+    public enum Tipo
+    {
+        Trailer,
+        Torton,
+        [Description("Doble remolque")]
+        Doble_Remolque,
+        Volteo,
+        [Description("Semi remolque")]
+        Semi_Remolque
+    }
+
+    public enum EstatusC
+    {
+        [Description("Mercancia OK")]
+        ok = 1,
+        [Description("Mercancia dañada")]
+        Danado = 2,
+        [Description("Mercancia Extraviada")]
+        Extraviado = 3
+    }
+}
